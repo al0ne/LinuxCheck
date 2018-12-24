17cdee0dbe60a11cd9d2de6f07798028f7776c69
al0ne <13449320+al0ne@users.noreply.github.com>
2018-12-24 16:21:55 +0800

    Update LinuxCheck.sh

--- LinuxCheck.sh
+++ LinuxCheck.sh
@@ -8,65 +8,65 @@ echo " # author：al0ne                    "
 echo " # https://github.com/al0ne/LinuxCheck     "
 echo -e "\n"
 if [ $UID -ne 0 ]; then
-    echo "请使用root权限运行！！！"
-    exit 1
+	echo "请使用root权限运行！！！"
+	exit 1
 fi
 source /etc/os-release
-if ag -V > /dev/null 2>&1; then
-    echo -n
+if ag -V >/dev/null 2>&1; then
+	echo -n
 else
-    case ${ID} in
-        debian|ubuntu|devuan)
-            apt-get -y install silversearcher-ag > /dev/null 2>&1
-                ;;
-        centos|fedora|rhel)
-            yum -y install the_silver_searcher > /dev/null 2>&1
-                ;;
-                        *)
-            exit 1
-                ;;
-    esac
+	case ${ID} in
+	debian | ubuntu | devuan)
+		apt-get -y install silversearcher-ag >/dev/null 2>&1
+		;;
+	centos | fedora | rhel)
+		yum -y install the_silver_searcher >/dev/null 2>&1
+		;;
+	*)
+		exit 1
+		;;
+	esac
 
 fi
 echo -e "\e[00;31m[+]系统改动\e[00m"
 case ${ID} in
-    debian|ubuntu|devuan)
-        apt install -y debsums > /dev/null 2>&1
-        debsums -e|ag -v 'OK'
-            ;;
-    centos|fedora|rhel)
-        rpm -Va
-            ;;
-                    *)
-        exit 1
-            ;;
-   esac
+debian | ubuntu | devuan)
+	apt install -y debsums >/dev/null 2>&1
+	debsums -e | ag -v 'OK'
+	;;
+centos | fedora | rhel)
+	rpm -Va
+	;;
+*)
+	exit 1
+	;;
+esac
+echo -e "\n"
 echo -e "\e[00;31m[+]系统信息\e[00m"
 #当前用户
-echo -e "USER:\t\t" `whoami` 2>/dev/null
+echo -e "USER:\t\t" $(whoami) 2>/dev/null
 #版本信息
-echo -e "OS Version:\t"  ${PRETTY_NAME}
+echo -e "OS Version:\t" ${PRETTY_NAME}
 #主机名
-echo -e "Hostname: \t" `hostname -s`
+echo -e "Hostname: \t" $(hostname -s)
 #uptime
-echo -e "uptime: \t" `uptime | awk -F ',' '{print $1}'`
+echo -e "uptime: \t" $(uptime | awk -F ',' '{print $1}')
 #cpu信息
-echo -e "CPU info:\t" `cat /proc/cpuinfo|ag -o '(?<=model name\t: ).*'|head -n 1`
+echo -e "CPU info:\t" $(cat /proc/cpuinfo | ag -o '(?<=model name\t: ).*' | head -n 1)
 # ipaddress
-ipaddress=`ifconfig|ag -o '(?<=inet addr:)\d+\.\d+\.\d+\.\d+'|ag -v '127.0.0.1'` >/dev/null 2>&1
-echo -e "IPADDR:\t\t${ipaddress}"|sed ":a;N;s/\n/ /g;ta"
+ipaddress=$(ifconfig | ag -o '(?<=inet addr:)\d+\.\d+\.\d+\.\d+' | ag -v '127.0.0.1') >/dev/null 2>&1
+echo -e "IPADDR:\t\t${ipaddress}" | sed ":a;N;s/\n/ /g;ta"
 echo -e "\n"
 
 echo -e "\e[00;31m[+]CPU使用率:  \e[00m"
-awk '$0 ~/cpu[0-9]/' /proc/stat 2>/dev/null  | while read line
-do
-    echo "$line" | awk '{total=$2+$3+$4+$5+$6+$7+$8;free=$5;\
+awk '$0 ~/cpu[0-9]/' /proc/stat 2>/dev/null | while read line; do
+	echo "$line" | awk '{total=$2+$3+$4+$5+$6+$7+$8;free=$5;\
         print$1" Free "free/total*100"%",\
         "Used " (total-free)/total*100"%"}'
 done
 echo -e "\n"
 #CPU占用
-cpu=`ps aux|grep -v ^'USER'|sort -rn -k3|head -10` 2>/dev/null
+cpu=$(ps aux | grep -v ^'USER' | sort -rn -k3 | head -10) 2>/dev/null
 echo -e "\e[00;31m[+]CPU TOP10:  \e[00m\n${cpu}\n"
 #内存占用
 echo -e "\e[00;31m[+]内存占用\e[00m"
@@ -77,7 +77,7 @@ echo -e "\e[00;31m[+]剩余空间\e[00m"
 df -mh
 echo -e "\n"
 echo -e "\e[00;31m[+]硬盘挂载\e[00m"
-cat /etc/fstab|ag -v "#"|awk '{print $1,$2,$3}'
+cat /etc/fstab | ag -v "#" | awk '{print $1,$2,$3}'
 echo -e "\n"
 #ifconfig
 echo -e "\e[00;31m[+]ifconfig\e[00m"
@@ -86,19 +86,18 @@ echo -e "\n"
 #网络流量
 echo -e "\e[00;31m[+]网络流量 \e[00m"
 echo "Interface    ByteRec   PackRec   ByteTran   PackTran"
-awk   ' NR>2' /proc/net/dev  | while read line
-do
-    echo "$line" | awk -F ':' '{print "  "$1"  " $2}' |\
-    awk '{print $1"   "$2 "    "$3"   "$10"  "$11}'
+awk ' NR>2' /proc/net/dev | while read line; do
+	echo "$line" | awk -F ':' '{print "  "$1"  " $2}' | \
+	awk '{print $1"   "$2 "    "$3"   "$10"  "$11}'
 done
 echo -e "\n"
 #端口监听
 echo -e "\e[00;31m[+]端口监听\e[00m"
-netstat -tulpen|ag 'tcp|udp.*' --nocolor
+netstat -tulpen | ag 'tcp|udp.*' --nocolor
 echo -e "\n"
 #网络连接
 echo -e "\e[00;31m[+]网络连接\e[00m"
-netstat -antop|ag ESTAB --nocolor
+netstat -antop | ag ESTAB --nocolor
 echo -e "\n"
 #路由表
 echo -e "\e[00;31m[+]路由表\e[00m"
@@ -106,11 +105,11 @@ echo -e "\e[00;31m[+]路由表\e[00m"
 echo -e "\n"
 #DNS
 echo -e "\e[00;31m[+]DNS Server\e[00m"
-cat /etc/resolv.conf|ag -o '\d+\.\d+\.\d+\.\d+' --nocolor
+cat /etc/resolv.conf | ag -o '\d+\.\d+\.\d+\.\d+' --nocolor
 echo -e "\n"
 #crontab
 echo -e "\e[00;31m[+]Crontab\e[00m"
-crontab -u root -l|ag -v '#' --nocolor
+crontab -u root -l | ag -v '#' --nocolor
 ls -al /etc/cron.*/*
 echo -e "\n"
 #env
@@ -119,11 +118,11 @@ env
 echo -e "\n"
 #passwd信息
 echo -e "\e[00;31m[+]可登陆用户\e[00m"
-cat /etc/passwd|ag -v 'nologin$|false$'
-echo -e "passwd文件修改日期:" `stat /etc/passwd|ag -o '(?<=Modify: ).*' --nocolor`
+cat /etc/passwd | ag -v 'nologin$|false$'
+echo -e "passwd文件修改日期:" $(stat /etc/passwd | ag -o '(?<=Modify: ).*' --nocolor)
 echo -e "\n"
-echo -e "\e[00;31m[+]sudoers(请注意NOPASSWD)\e[00m" 
-cat /etc/sudoers|ag -v '#'|sed -e '/^$/d'|ag ALL --nocolor
+echo -e "\e[00;31m[+]sudoers(请注意NOPASSWD)\e[00m"
+cat /etc/sudoers | ag -v '#' | sed -e '/^$/d' | ag ALL --nocolor
 echo -e "\n"
 #防火墙
 echo -e "\e[00;31m[+]IPTABLES防火墙\e[00m"
@@ -136,27 +135,28 @@ echo -e "\n"
 last
 echo -e "\n"
 lastlog
-echo "登陆ip:" `ag -a accepted /var/log/auth.*|ag -o '\d+\.\d+\.\d+\.\d+'|sort|uniq`
+echo "登陆ip:" $(ag -a accepted /var/log/auth.* | ag -o '\d+\.\d+\.\d+\.\d+' | sort | uniq)
 echo -e "\n"
 #运行服务
 echo -e "\e[00;31m[+]Service \e[00m"
 case ${ID} in
-    debian|ubuntu|devuan)
-        service --status-all |ag -Q '+' --nocolor
-            ;;
-    centos|fedora|rhel)
-        service --status-all |ag -Q 'is running' --nocolor
-            ;;
-                    *)
-     	exit 1
-            ;;
-   esac
+debian | ubuntu | devuan)
+	service --status-all | ag -Q '+' --nocolor
+	;;
+centos | fedora | rhel)
+	service --status-all | ag -Q 'is running' --nocolor
+	;;
+*)
+	exit 1
+	;;
+esac
 echo -e "\n"
 #查看history文件
 echo -e "\e[00;31m[+]History\e[00m"
-ls -la ~/.*_history; ls -la /root/.*_history
+ls -la ~/.*_history
+ls -la /root/.*_history
 echo -e "\n"
-cat ~/.*history|ag '[12](?:[0-9]{1,2})?\.[12](?:[0-9]{1,2})?\.[12](?:[0-9]{1,2})?\.[12](?:[0-9]{1,2})?|http://'
+cat ~/.*history | ag '[12](?:[0-9]{1,2})?\.[12](?:[0-9]{1,2})?\.[12](?:[0-9]{1,2})?\.[12](?:[0-9]{1,2})?|http://'
 echo -e "\n"
 #HOSTS
 echo -e "\e[00;31m[+]/etc/hosts \e[00m"
@@ -164,19 +164,19 @@ cat /etc/hosts | ag -v "#"
 echo -e "\n"
 #/etc/profile
 echo -e "\e[00;31m[+]/etc/profile \e[00m"
-cat /etc/profile|ag -v '#'
+cat /etc/profile | ag -v '#'
 echo -e "\n"
 #~/.bash_profile
 echo -e "\e[00;31m[+]~/.bash_profile \e[00m"
-cat ~/.bash_profile|ag -v '#'
+cat ~/.bash_profile | ag -v '#'
 echo -e "\n"
 #~/.bashrc
 echo -e "\e[00;31m[+]~/.bashrc \e[00m"
-cat ~/.bashrc|ag -v '#'
+cat ~/.bashrc | ag -v '#'
 echo -e "\n"
 #bash反弹shell
 echo -e "\e[00;31m[+]bash反弹shell \e[00m"
-ps -ef | ag 'bash -i'|ag -v 'ag'|awk '{print $2}'|xargs -i{} lsof -p {}|ag 'ESTAB' --nocolor
+ps -ef | ag 'bash -i' | ag -v 'ag' | awk '{print $2}' | xargs -i{} lsof -p {} | ag 'ESTAB' --nocolor
 echo -e "\n"
 #...隐藏文件
 echo -e "\e[00;31m[+]...隐藏文件 \e[00m"
@@ -192,11 +192,11 @@ lsof +L1
 echo -e "\n"
 #近7天改动
 echo -e "\e[00;31m[+]近七天文件改动 \e[00m"
-find /etc /bin /sbin /dev /root/ /home /tmp  -mtime -7|ag -v 'cache|vim'
+find /etc /bin /sbin /dev /root/ /home /tmp -mtime -7 | ag -v 'cache|vim'
 echo -e "\n"
 #大文件>200mb
 echo -e "\e[00;31m[+]大文件>200mb \e[00m"
-find / ! -path "/proc/*" ! -path "/sys/*" ! -path "/run/*" ! -path "/boot/*" -size +200M -print  2>/dev/null|xargs -i{} ls -alh {}|ag '\.jpg|\.png|\.zip|\.tar.gz|\.tgz|\.7z|\.log|\.xz|\.rar|\.bak|\.old|\.sql|\.txt|\.tar|/\w+$' --nocolor
+find / ! -path "/proc/*" ! -path "/sys/*" ! -path "/run/*" ! -path "/boot/*" -size +200M -print 2>/dev/null | xargs -i{} ls -alh {} | ag '\.jpg|\.png|\.zip|\.tar.gz|\.tgz|\.7z|\.log|\.xz|\.rar|\.bak|\.old|\.sql|\.txt|\.tar|/\w+$' --nocolor
 echo -e "\n"
 #lsmod 查看模块
 echo -e "\e[00;31m[+]lsmod模块\e[00m"
@@ -205,30 +205,32 @@ echo -e "\n"
 #检查ssh key
 echo -e "\e[00;31m[+]SSH key\e[00m"
 sshkey=${HOME}/.ssh/authorized_keys
-if [ -e "${sshkey}" ]; then 
-    cat ${sshkey}
+if [ -e "${sshkey}" ]; then
+	cat ${sshkey}
 else
-    echo -e "SSH key文件不存在\n"
-fi 
+	echo -e "SSH key文件不存在\n"
+fi
 echo -e "\n"
+#PHP webshell查杀
+echo -e "\e[00;31m[+]PHP webshell查杀\e[00m"
+ag --php -l -s 'assert\(|phpspy|c99sh|milw0rm|eval?\(|\(gunerpress|\(base64_decoolcode|spider_bc|shell_exec\(|passthru\(|base64_decode\s?\(|gzuncompress\s?\(|\(\$\$\w+|call_user_func\(|preg_replace_callback\(|preg_replace\(|register_shutdown_function\(|register_tick_function\(|mb_ereg_replace_callback\(|filter_var\(|ob_start\(|usort\(|uksort\(|GzinFlate\s?\(|\$\w+\(\d+\)\.\$\w+\(\d+\)\.|\$\w+=str_replace\(' /
+echo -e "\n"
+rkhuntercheck() {
+	rkhunter='/tmp/rkhunter-1.4.6/files/rkhunter'
+	if ${rkhunter} >/dev/null 2>&1; then
+		${rkhunter} --checkall --sk | ag -v 'OK|Not found|None found'
+	else
+		wget 'https://astuteinternet.dl.sourceforge.net/project/rkhunter/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz' -O /tmp/rkhunter.tar.gz >/dev/null 2>&1
+		cd /tmp
+		tar -zxvf rkhunter.tar.gz >/dev/null 2>&1
+		${rkhunter} --checkall --sk | ag -v 'OK|Not found|None found'
 
-rkhuntercheck()
-{
-rkhunter='/tmp/rkhunter-1.4.6/files/rkhunter'
-if ${rkhunter} > /dev/null 2>&1; then
-    ${rkhunter} --checkall --sk|ag -v 'OK|Not found|None found'
-else
-    wget 'https://astuteinternet.dl.sourceforge.net/project/rkhunter/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz' -O /tmp/rkhunter.tar.gz > /dev/null 2>&1
-    cd /tmp
-    tar -zxvf rkhunter.tar.gz > /dev/null 2>&1
-    ${rkhunter} --checkall --sk|ag -v 'OK|Not found|None found'
-
-fi  
+	fi
 }
-ping -c 1 114.114.114.114 > /dev/null 2>&1
-if [ $? -eq 0 ];then
-    echo -e "\e[00;31m[+]RKhunter\e[00m"
-    rkhuntercheck
+ping -c 1 114.114.114.114 >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	echo -e "\e[00;31m[+]RKhunter\e[00m"
+	rkhuntercheck
 else
-    echo -e '\n'
+	echo -e '\n'
 fi
